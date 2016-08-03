5cf46401a0cb0dcee5fa811c1dd7ea6785b6a3ec
Steve McKeogh <steve.mckeogh@gmail.com>
2016-08-03 19:51:28 +0000

    Quick links

--- src/resources/views/admin/dashboard/index.blade.php
+++ src/resources/views/admin/dashboard/index.blade.php
@@ -15,7 +15,14 @@
     {{ CmsForm::success() }}
     
     <div class="Box">
-        Quick links to appear here... at some point.        
+         <ul class="Quicklinks">
+            @foreach (CmsYaml::getNav() as $idx => $nav)
+                 <li class="Quicklinks__item"><a href="{{ $nav['url'] }}" class="Quicklinks__link Utility--valign-middle"><span>
+                     <i class="fa fa-{{ $nav['icon'] }}"></i>
+                     <h2 class="Quicklinks__title">{{ $nav["title"] }}</h2>
+                 </span></a></li>
+             @endforeach
+         </ul>     
     </div>
         
 @endsection
