48b4802ce0b9ed6b0c94f2bc9ace3da8cbce02b8
Elliott Landsborough <elliott.landsborough@codegent.com>
2016-08-10 11:25:35 +0100

    Api Controller with pagination

---
+++ src/app/Http/Controllers/Core/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Thinmartian\Cms\App\Http\Controllers\Core;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Pagination\Paginator
+
+class ApiController extends Controller
+{
+    public function get($id = null, $model = null) {
+        if (class_exists($model)) {
+            $model = $model::find($id);
+            return $model;
+        }
+    }
+
+    public function getAll($model = null) {
+        if (class_exists($model)) {
+            $model = $model::paginate(10)->getCollection();
+            return $model;
+        }
+    }
+}
